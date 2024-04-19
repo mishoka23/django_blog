6cb5a9ce537866631d48adab90b77a33c11f4f87
Mihail Stefanov <mihail.n.stefanov@gmail.com>
2024-04-19 16:44:08 +0300

    mysqlite3 added

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,16 @@
+from .models import Post, Author, Tag
 from django.contrib import admin
 
-# Register your models here.
+
+# Register your models here
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
