ceb8b398234ff35f4e79c807f0a25f58cdf34af8
MihailStefanov <stefanov.mihail@abv.bg>
2024-04-26 10:59:56 +0300

    image uploads (before Class based Views)

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -9,7 +9,7 @@ class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
     def __str__(self):
-      return self.caption
+        return self.caption
 
 
 class Author(models.Model):
@@ -27,7 +27,8 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    # image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -5,27 +5,27 @@ from .models import Post
 # Create your views here.
 
 
-
 def get_date(post: dict):
     return post.get("date")
+
 
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
-      "posts": latest_posts
+        "posts": latest_posts
     })
 
 
 def posts(request):
     all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
-      "all_posts": all_posts
+        "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-      "post": identified_post,
-      "post_tags": identified_post.tags.all()
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
