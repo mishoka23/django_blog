ac67e52988bcaae382b8f00e629b99c96da9b339
MihailStefanov <stefanov.mihail@abv.bg>
2024-04-26 13:12:52 +0300

    Final (ready for deployment)

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.core.validators import MinLengthValidator
 
@@ -27,7 +26,6 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    # image_name = models.CharField(max_length=100)
     image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
@@ -35,3 +33,14 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
